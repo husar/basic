6584f050946df1a5d958c69b058d4b2a159ca6f0
Ján Štucka <44474224+husar@users.noreply.github.com>
2022-01-08 09:29:23 +0100

    pagination

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -32,10 +32,10 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @php($i = 1)
+                                <!-- @php($i = 1) -->
                                 @foreach($categories as $category)
                                     <tr>
-                                        <th scope="row">{{ $i++ }}</th>
+                                        <th scope="row">{{ $categories->firstItem()+$loop->index }}</th>
                                         <td>{{ $category->category_name }}</td>
                                         <td>{{ $category->user_id }}</td>
                                         <td>
@@ -49,6 +49,7 @@
                                 @endforeach
                             </tbody>
                         </table>
+                        {{ $categories->links() }}
                     </div>
                 </div>
                 <div class="col-md-4">
