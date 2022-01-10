d8d254ad78fa439a3872d8d121d8da90731170df
Ján Štucka <44474224+husar@users.noreply.github.com>
2022-01-10 21:49:49 +0100

    Updating category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,12 +11,12 @@ use Illuminate\Support\Facades\DB;
 class CategoryController extends Controller
 {
     public function AllCat(){
-        $categories = DB::table('categories')
-                    ->join('users','categories.user_id','users.id')
-                    ->select('categories.*','users.name')
-                    ->latest()->paginate(5);
+        // $categories = DB::table('categories')
+        //             ->join('users','categories.user_id','users.id')
+        //             ->select('categories.*','users.name')
+        //             ->latest()->paginate(5);
 
-        //$categories = Category::latest()->paginate(5);
+        $categories = Category::latest()->paginate(5);
         //$categories = DB::table('categories')->latest()->paginate(5);
         return view('admin.category.index', compact('categories'));
     }
@@ -50,4 +50,18 @@ class CategoryController extends Controller
         return Redirect()->back()->with('success', 'Category inserted successfully.');
 
     }
+
+    public function Edit($id){
+        $categories = Category::find($id);
+        return view('admin.category.edit', compact('categories'));
+    }
+
+    public function Update(Request $request ,$id){
+        $update = Category::find($id)->update([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id
+        ]);
+        return Redirect()->route('all.category')->with('success', 'Category updated successfully.');
+    }
+
 }
