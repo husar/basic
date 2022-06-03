b528e0bc057e8b01bf413e6b3848204ac6643e4b
husar robota <stuckajano6@gmail.com>
2022-06-03 08:43:18 +0200

    CRUD about part

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\HomeAbout;
+use Illuminate\Support\Carbon;
+
+class AboutController extends Controller
+{
+    public function HomeAbout(){
+        $homeabout = HomeAbout::latest()->get();
+        return view('admin.home.index', compact('homeabout'));
+    }
+
+    public function AddAbout(){
+        return view('admin.home.create');
+    }
+
+    public function StoreAbout(Request $request){
+
+        HomeAbout::insert([
+            'title'     => $request->title,
+            'short_des' => $request->short_des,
+            'long_des'  => $request->long_des,
+            'created_at'=> Carbon::now()
+        ]);
+
+        return Redirect()->route('home.about')->with('success', 'About inserted successfully');
+    }
+
+    public function EditAbout($id){
+        $homeabout = HomeAbout::find($id);
+        return view('admin.home.edit',compact('homeabout'));
+    }
+
+    public function UpdateAbout(Request $request, $id){
+        $update = HomeAbout::find($id)->update([
+            'title'     => $request->title,
+            'short_des' => $request->short_des,
+            'long_des'  => $request->long_des
+        ]);
+
+        return Redirect()->route('home.about')->with('success', 'About updated successfully');
+    }
+
+    public function DeleteAbout($id){
+        $delete = HomeAbout::find($id)->Delete();
+        return Redirect()->route('home.about')->with('danger', 'About deleted successfully');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
@@ -24,7 +25,8 @@ Route::get('/email/verify', function () {
 
 Route::get('/', function () {
     $brands = DB::table('brands')->get();
-    return view('home', compact('brands'));
+    $abouts = DB::table('home_abouts')->first();
+    return view('home', compact('brands','abouts'));
 });
 
 Route::get('/home', function () {
@@ -78,6 +80,20 @@ Route::get('/home/slider', [HomeController::class, 'HomeSlider'])->name('home.sl
 Route::get('/add/slider', [HomeController::class, 'AddSlider'])->name('add.slider');
 
 Route::post('/store/slider', [HomeController::class, 'StoreSlider'])->name('store.slider');
+
+//Home About All Route
+
+Route::get('/home/About', [AboutController::class, 'HomeAbout'])->name('home.about');
+
+Route::get('/add/About', [AboutController::class, 'AddAbout'])->name('add.about');
+
+Route::post('/store/About', [AboutController::class, 'StoreAbout'])->name('store.about');
+
+Route::get('/about/edit/{id}', [AboutController::class, 'EditAbout']);
+
+Route::post('/update/homeabout/{id}', [AboutController::class, 'UpdateAbout']);
+
+Route::get('/about/delete/{id}', [AboutController::class, 'DeleteAbout']);
 
 // For User
 
