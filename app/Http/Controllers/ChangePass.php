7867cc141517eb8ed7c68e155deb8cb2065f1bf1
husar robota <stuckajano6@gmail.com>
2022-07-26 13:43:27 +0200

    Finished updatin password

--- app/Http/Controllers/ChangePass.php
+++ app/Http/Controllers/ChangePass.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ChangePass extends Controller
 {
@@ -14,9 +17,28 @@ class ChangePass extends Controller
     }
 
 
-    public function UpdatePassword(){
+    public function UpdatePassword(Request $request){
 
-        
+        $validateData = $request->validate([
+            'oldpassword'   => 'required',
+            'password'      => 'required|confirmed'
+        ]);
+
+        $hashedPassword = Auth::user()->password;
+
+        if(Hash::check($request->oldpassword,$hashedPassword)){
+
+            $user               = User::find(Auth::id());
+            $user->password  = Hash::make($request->password);
+            $user->save();
+            Auth::logout();
+            return redirect()->route('login')->with('success','Password Is Changed Successfuly');
+
+        }else{
+
+            return redirect()->back()->with('error','Current Password Is Invalid');
+
+        }
 
     }
 
--- resources/views/admin/body/change_password.blade.php
+++ resources/views/admin/body/change_password.blade.php
@@ -7,27 +7,27 @@
         <h2>Change Password</h2>
     </div>
     <div class="card-body">
-        <form class="form-pill" method="POST" action="{{ route(password.update) }}">
+        <form class="form-pill" method="POST" action="{{ route('password.update') }}">
             @csrf
             <div class="form-group">
                 <label for="exampleFormControlPassword">Current Password</label>
                 <input type="password" class="form-control" id="current_password" placeholder="Current Password" name="oldpassword">
                 @error('oldpassword')
-                <span class="test-danger">{{ $message }}</span>
+                <span class="text-danger">{{ $message }}</span>
                 @enderror
             </div>
             <div class="form-group">
                 <label for="exampleFormControlPassword">New Password</label>
                 <input type="password" class="form-control" id="password" placeholder="New Password" name="password">
                 @error('password')
-                <span class="test-danger">{{ $message }}</span>
+                <span class="text-danger">{{ $message }}</span>
                 @enderror
             </div>
             <div class="form-group">
                 <label for="exampleFormControlPassword">Confirm Password</label>
                 <input type="password" class="form-control" id="password_confirmation" placeholder="Confirm Password" name="password_confirmation">
                 @error('password_confirmation')
-                <span class="test-danger">{{ $message }}</span>
+                <span class="text-danger">{{ $message }}</span>
                 @enderror
             </div>
             <button type="submit" class="btn btn-primary btn-default"> Save</button>
